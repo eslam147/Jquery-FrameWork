bc2f1a0f2e57635fbf4ed53f756c8b080d8c6fa7
Islam Mohamed <emomemo4555@gmail.com>
2025-12-10 12:21:35 +0200

    upload first Jquery FrameWork Like Laravel MVC

--- routes/web.js
+++ routes/web.js
@@ -3,24 +3,20 @@
  * routes/web.js
  * 
  * Define all AJAX routes here using Laravel-like syntax:
- * Route::get(url, [ControllerName::class, 'methodName'], elementReturn);
- * Route::post(url, [ControllerName::class, 'methodName'], elementReturn);
- * 
- * Or use directly with Ajax:
- * Ajax::get(url, [ControllerName::class, 'methodName'], elementReturn);
- * Ajax::post(url, [ControllerName::class, 'methodName'], elementReturn, data);
+ * Route::get(url, [ControllerName::class, 'methodName']);
+ * Route::post(url, [ControllerName::class, 'methodName']);
  */
 
-Route::get('https://jsonplaceholder.typicode.com/posts/1', [AjaxGetController::class, 'onClick'], '#ajax-result');
+Route::get('https://jsonplaceholder.typicode.com/posts/1', [AjaxGetController::class, 'onClick']);
 
 // POST Request Route
-Route::post('https://jsonplaceholder.typicode.com/posts', [AjaxPostController::class, 'onClick'], '#ajax-result');
+Route::post('https://jsonplaceholder.typicode.com/posts', [AjaxPostController::class, 'onClick']);
 
 
 // Log all registered routes
 
 // You can add more routes here:
-// Route::get('/api/users', [UserController::class, 'index'], '#users-list');
-// Route::post('/api/users', [UserController::class, 'store'], '#form-result');
-// Route::put('/api/users/:id', [UserController::class, 'update'], '#form-result');
-// Route::delete('/api/users/:id', [UserController::class, 'destroy'], '#form-result');
+// Route::get('/api/users', [UserController::class, 'index']);
+// Route::post('/api/users', [UserController::class, 'store']);
+// Route::put('/api/users/:id', [UserController::class, 'update']);
+// Route::delete('/api/users/:id', [UserController::class, 'destroy']);
